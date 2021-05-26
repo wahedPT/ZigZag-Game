3dcfad38dd6b6fbf3510da64ff96f05c224fdd28
wahedxcube <wahedalikhan.md@xcubelabs.com>
2021-05-26 15:18:38 -0700

    Player movement done

---
+++ Assets/PMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PMovement : MonoBehaviour
+{
+    public int speed;
+    [SerializeField]
+    Vector3 direction;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(direction==Vector3.forward)
+            {
+                direction = Vector3.left;
+            }
+            else
+            {
+                direction = Vector3.forward;
+            }
+           
+        }
+        transform.Translate(direction * speed * Time.deltaTime);
+
+
+    }
+}
